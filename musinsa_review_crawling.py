1b3988150b27adc1a900bed7c0a77d7714c0dd81
Jake-Dong <72381484+Jake-Dong@users.noreply.github.com>
2020-10-05 18:08:02 +0900

    Update musinsa_review_crawling.py

--- musinsa_review_crawling.py
+++ musinsa_review_crawling.py
@@ -69,7 +69,6 @@ def get_shoes_review():
     
     style_list = ['photo','goods']
 
-
     for style in style_list:
     
         musinsa_rvw_list = []
@@ -94,7 +93,7 @@ def get_shoes_review():
                     img_url = img_src.get_attribute('src')
                     img_url_list.append(img_url)
                 try:
-                no_data = driver.find_element_by_class_name('mypage_review_none')
+                    no_data = driver.find_element_by_class_name('mypage_review_none')
                     if no_data != None:
                         break
 
