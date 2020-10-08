5a42440a36f4510c8b04ced64d401292a858053d
Jake-Dong <72381484+Jake-Dong@users.noreply.github.com>
2020-10-08 18:15:24 +0900

    무신사 웹 구조 변경으로 href에서 title 로 변경

--- musinsa_review_crawling.py
+++ musinsa_review_crawling.py
@@ -46,11 +46,11 @@ def get_shoes_info(category, page, **kwargs):
     driver.get(url)
     time.sleep(60)
     prod_brand = driver.find_elements_by_css_selector('#searchList > li > div.li_inner > div.article_info > p.item_title > a')
-    prod_id_list = driver.find_elements_by_css_selector('#searchList > li > div.li_inner > div.list_img > a')
+    prod_id_list = driver.find_elements_by_css_selector('#searchList > li > div.li_inner > div.article_info > p.list_info > alist_info')
     prod_name_list = driver.find_elements_by_css_selector('#searchList > li > div.li_inner > div.article_info > p.list_info > a')
         
     for q,w,e in zip(prod_id_list,prod_name_list,prod_brand):
-        raw_prod_id = q.get_attribute("href")
+        raw_prod_id = q.get_attribute("title")
         prod_name = w.text
         prod_brand = e.text
         prod_id = raw_prod_id.split('/')[6]
