5f0b24934aceff14a65d5b3b1f0921d856375f89
kadarin123 <kadarin123@naver.com>
2020-10-09 01:36:15 +0900

    옵션추가

--- musinsa_review_crawling.py
+++ musinsa_review_crawling.py
@@ -41,6 +41,7 @@ def get_shoes_info(category, page, **kwargs):
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36')
     driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=options)
     
@@ -103,6 +104,7 @@ def get_shoes_review(category, **kwargs):
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-gpu")
+    options.add_argument('--disable-dev-shm-usage')
     driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',options=options)
     
     style_list = ['photo','goods']
