c5a5e0dc12926527ce5094c4af556bb6cf853ab6
ShinyoungAshleyChoi <72444304+ShinyoungAshleyChoi@users.noreply.github.com>
2020-10-13 18:01:58 +0900

    오타수정
    응..

--- to_sql.py
+++ to_sql.py
@@ -1,10 +1,12 @@
 import pymysql
+import numpy as np
+import pandas as pd
+
 from sqlalchemy import create_engine
 pymysql.install_as_MySQLdb()
 import MySQLdb
 
-import numpy as np
-import pandas as pdd
+
 
 danawa = pd.read_csv('f'/root/reviews/danawa_{b_name}_id.csv', index_col=0, thousands=',')
 del danawa['prod_info']
