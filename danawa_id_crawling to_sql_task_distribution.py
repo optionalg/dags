eb58db39df783b8f5f8ec1e75bdaca48adfedb29
kadarin123 <67451493+kadarin123@users.noreply.github.com>
2020-10-23 17:03:29 +0900

    대기시간 수정 & 오류나도 재시도 x

--- danawa_id_crawling to_sql_task_distribution.py
+++ danawa_id_crawling to_sql_task_distribution.py
@@ -61,8 +61,8 @@ def get_shoes_info(b_name, page):
     for i in range(1,16):
         url = 'http://search.danawa.com/dsearch.php?query=%EC%8B%A0%EB%B0%9C&originalQuery=%EC%8B%A0%EB%B0%9C&previousKeyword=%EC%8B%A0%EB%B0%9C&volumeType=allvs&page='+str(i)+'&limit=120&sort=opinionDESC&list=list&boost=true&addDelivery=N&brand='+str(page)+'&tab=main'
         driver.get(url)
-        time.sleep(2)
-        driver.implicitly_wait(20)
+        time.sleep(3)
+        driver.implicitly_wait(60)
         try:
             nosearchArea = driver.find_element_by_selector('#nosearchArea')
             break
@@ -253,8 +253,6 @@ default_args = {
     'depends_on_past': False,
     'start_date': datetime(2020, 10, 1, tzinfo=local_tz),
     'catchup': False,
-    'retries': 2,
-    'retry_delay':timedelta(minutes=1)
 }    
     
 # DAG인스턴스 생성
