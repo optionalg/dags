e8f3060a61acfd094a8c9a727b1c74df0d5d33b2
kadarin123 <67451493+kadarin123@users.noreply.github.com>
2020-10-23 16:31:59 +0900

    시간 늘리기

--- musinsa_id_crawling to_sql_task_distribution.py
+++ musinsa_id_crawling to_sql_task_distribution.py
@@ -55,7 +55,7 @@ def get_shoes_info(category, page):
     url = 'https://store.musinsa.com/app/items/lists/'+str(page)+'/?category=&d_cat_cd=005&u_cat_cd=&brand=&sort=pop&sub_sort=&display_cnt=3000&page=1&page_kind=category&list_kind=small&free_dlv=&ex_soldout=N&sale_goods=&exclusive_yn=&price=&color=&a_cat_cd=&size=&tag=&popup=&brand_favorite_yn=&goods_favorite_yn=&blf_yn=&campaign_yn=&bwith_yn=&price1=&price2=&chk_soldout=on'
     driver.get(url)
     time.sleep(10)
-    drvier.implicitly_wait(60)
+    driver.implicitly_wait(60)
     prod_id_list = driver.find_elements_by_css_selector('#searchList > li > div.li_inner > div.list_img > a > img')
     for q in prod_id_list:
         raw_prod_id = q.get_attribute("data-original")
