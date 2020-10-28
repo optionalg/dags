ee4711692bf7f17a6e49637df899e9b33cc6cf29
kadarin123 <67451493+kadarin123@users.noreply.github.com>
2020-10-28 12:57:54 +0900

    conn수정

--- DANAWA_ID_CRAWLING_DAG.py
+++ DANAWA_ID_CRAWLING_DAG.py
@@ -137,8 +137,7 @@ def get_shoes_info(b_name, page, **kwargs):
     # 마리아디비로 전송
     conn = pymysql.connect(host='35.185.210.97', port=3306, user='footfootbig', password='footbigmaria!', database='footfoot')
     try:
-        with conn.cursor() as curs:
-            danawa.to_sql(name='danawa_shoes', con=curs, if_exists='append', index=False)
+        danawa.to_sql(name='danawa_shoes', flavor='mysql', con=conn, if_exists='append', index=False)
     finally:
         conn.close()
 
